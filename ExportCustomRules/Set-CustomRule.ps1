935f0d36c7bbdab5f9ae1cbfa9ec66d0d56aaade
miltonb <59422970+miltonms@users.noreply.github.com>
2020-05-22 13:54:59 -0600

    Create Set-CustomRule.ps1

---
+++ ExportCustomRules/Set-CustomRule.ps1
@@ -0,0 +1,37 @@
+﻿#Requires -modules ADSync
+
+#Use this script on the target computer to replace the source computer connector Ids in the .ps1 files with the ones on the target computer.
+#Use this script if script ExportCustomRules.ps1 https://github.com/mbendana/ADConnect/blob/ExportCustomRules/ExportCustomRules/Export-CustomRule.ps1 did not complete successfully.
+
+#Check if the ADSync module is loaded. If not, import it.
+if (-not (Get-Module -Name ADSync)){
+    Import-Module "C:\Program Files\Microsoft Azure AD Sync\Bin\ADSync\ADSync.psd1"
+}
+
+foreach($folder in (Get-ChildItem -Path "$HOME\Desktop\ADConnectExportedCustomRules\" -Directory)){
+    $sourceConnectorId = $folder.Name.split("_")[2]
+    $sourceConnectorName = $folder.Name.split("_")[0]
+    try{
+        if($sourceConnectorName -eq "Cloud"){
+            $targetConnector = Get-ADSyncConnector | Where-Object { ($_.Name -like "*AAD") -and ($_.ConnectorTypeName -eq "Extensible2")}
+            $targetConnectorId = $targetConnector.Identifier
+        }else{
+            $targetConnector = Get-ADSyncConnector -Name $sourceConnectorName
+            $targetConnectorId = $targetConnector.Identifier
+        }
+    }
+    catch{
+        Write-Warning "No connector with name $sourceConnectorName found on target computer $env:COMPUTERNAME"
+    }
+            
+    $files = Get-ChildItem -Path $folder.FullName -Filter "*.ps1" -File
+
+    foreach ($file in $files){
+        $parentPath = $file.PSParentPath
+        $fileName = $file.Name
+
+        $fileContent = (Get-Content $file.FullName -raw) -replace $sourceConnectorId, $targetConnectorId
+
+        Set-Content -Value $fileContent -Path $file.FullName
+    }
+}
